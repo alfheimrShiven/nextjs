940564e9278bb9cded2e646cfb49df587cd68177
Shivendra Singh <shiven@Shivendras-MacBook-Pro.local>
2022-03-03 20:49:53 +0530

    Added a route

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
+import Date from '../components/date'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts' 
 
@@ -25,9 +26,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className= {utilStyles.listItem} key= {id}>
+              <Link href= {`/posts/${id}`}>
               {title}
+              </Link>
               <br />
+              <small className={utilStyles.lightText}>
               {date}
+              </small>
             </li>
           ))}
         </ul>
